ed720f4981e4faa9c99184af0a2efd4877adea4f
Jon-Stickrod <jonstickrodCodes@gmail.com>
2020-10-19 19:17:10 -0700

    auth endpoints working

--- api/server.js
+++ api/server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 
-
+const authRouter = require("../auth/auth-router.js");
 
 const server = express();
+
+
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+
+server.use("/api/auth", authRouter);
 
 server.get("/", (req,res) => {
     res.json({api: "up"});
